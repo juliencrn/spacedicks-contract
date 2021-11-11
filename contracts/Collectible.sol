75c8132de30c7aaa33f6fb702e08a5a1eb712186
Julien <juliencaron@protonmail.com>
2021-11-11 06:08:39 +0100

    feat(nft): Build smart contract

--- contracts/Collectible.sol
+++ contracts/Collectible.sol
@@ -1,7 +1,97 @@
-pragma solidity >=0.5.0 <0.9.0;
+pragma solidity >=0.8.3 <0.9.0;
 
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract Collectible is ERC721 {
-    constructor() ERC721("Collectible", "CLTB") {}
+contract Collectible is Ownable, ERC721 {
+    // Collection options
+    uint256 public totalSupply;
+    uint256 public nextCollectibleId;
+    string private _currentBaseURI;
+
+    // Store NFT properties
+    struct NFTProperties {
+        // TODO: Can I use a enum instead? [red, blue, etc]
+        uint8 color;
+        // TODO: If it's unnecessary, remove string prop, it takes memory.
+        string text;
+    }
+    mapping(uint256 => NFTProperties) idToProperties;
+
+    constructor() ERC721("Collectible", "COLL") {
+        totalSupply = 10_000;
+        nextCollectibleId = 0;
+
+        // TODO: Replace by API URI when available using ENV variable
+        setBaseURI("http://localhost:5000/");
+
+        // Mint my own NFT tokens.
+        mint(5, "First NFT");
+        mint(1, "Second NFT");
+    }
+
+    // Claim is the public function to mint an NFT, it uses mint() internaly.
+    function claim(uint8 _color, string memory _text) external payable {
+        require(
+            msg.value == 0.01 ether,
+            "Claiming a Collectible costs 0.01 ether"
+        );
+
+        mint(_color, _text);
+
+        // Give 0.01 ether to the contract owner
+        payable(owner()).transfer(0.01 ether);
+    }
+
+    function mint(uint8 _color, string memory _text) internal {
+        require(nextCollectibleId < totalSupply);
+
+        uint256 tokenId = nextCollectibleId;
+        nextCollectibleId++;
+
+        idToProperties[tokenId] = NFTProperties(_color, _text);
+        _safeMint(msg.sender, tokenId);
+    }
+
+    function setBaseURI(string memory baseURI) public onlyOwner {
+        _currentBaseURI = baseURI;
+    }
+
+    function _baseURI() internal view virtual override returns (string memory) {
+        return _currentBaseURI;
+    }
+
+    function get(uint256 _tokenId)
+        external
+        view
+        returns (
+            uint256 id,
+            uint8 color,
+            string memory text
+        )
+    {
+        require(_exists(_tokenId), "Token not minted yet");
+        NFTProperties memory properties = idToProperties[_tokenId];
+        id = _tokenId;
+        color = properties.color;
+        text = properties.text;
+    }
+
+    // Example of update NFT property
+    function changeText(uint256 _tokenId, string memory _newText) public {
+        require(_exists(_tokenId), "Token not minted yet");
+        require(
+            ownerOf(_tokenId) == msg.sender,
+            "Only the owner of this NFT can change its text"
+        );
+
+        idToProperties[_tokenId].text = _newText;
+    }
+
+    function getText(uint256 _tokenId) external view returns (string memory) {
+        require(_exists(_tokenId), "Token not minted yet");
+
+        NFTProperties memory properties = idToProperties[_tokenId];
+        return properties.text;
+    }
 }
--- test/Collectible.test.js
+++ test/Collectible.test.js
@@ -1,19 +1,146 @@
-const Collectible = artifacts.require("Collectible.sol");
 const assert = require("chai").assert;
 const truffleAssert = require('truffle-assertions');
 
+const Collectible = artifacts.require("Collectible.sol");
+
 // Test helpers - https://docs.openzeppelin.com/test-helpers/0.5/api
-// const { expectRevert, expectEvent, time } = require('@openzeppelin/test-helpers');
+const { expectRevert } = require('@openzeppelin/test-helpers');
 
-contract('Election', accounts => {
-    describe("Test the contract", () => {
-        // beforeEach(() => {})
-        // beforeAll(() => {})
+contract('Collectible', accounts => {
+    let collectible
+    before(async () => {
+        collectible = await Collectible.deployed()
+    })
 
-        it("should test", async () => {
-            const collectible = await Collectible.deployed()
+    describe("Deployed Collectible Contract", () => {
+        it("has an owner", async () => {
+            let owner = await collectible.owner()
+            expect(owner).to.equal(accounts[0])
+        })
 
-            // Tests...
+        it("has a name", async () => {
+            let name = await collectible.name()
+            expect(name).to.equal("Collectible")
+        })
+
+        it("has a symbol", async () => {
+            let symbol = await collectible.symbol()
+            expect(symbol).to.equal("COLL")
+        })
+
+        it("has correct tokenURI", async () => {
+            let tokenURI = await collectible.tokenURI(1)
+            expect(tokenURI).to.equal("http://localhost:5000/1")
+        })
+
+        it("has correct total supply", async () => {
+            let totalSupply = await collectible.totalSupply()
+            expect(web3.utils.toNumber(totalSupply)).to.equal(10_000)
+        })
+
+        it("gifts the owner the origin of collectible at least", async () => {
+            let owner = await collectible.ownerOf(0)
+            expect(owner).to.equal(accounts[0])
+
+            let properties = await collectible.get(0)
+            expect(properties.id.toNumber()).to.equal(0)
+            expect(properties.color.toNumber()).to.equal(5)
+            expect(properties.text.toString()).to.equal("First NFT")
+        })
+    })
+
+    let price = web3.utils.toBN(web3.utils.toWei('0.01', 'ether'))
+
+    describe("Minting a collectible", () => {
+        let ownerBalanceBefore
+        let buyerBalanceBefore
+
+        before(async () => {
+            ownerBalanceBefore = await web3.eth.getBalance(accounts[0]);
+            ownerBalanceBefore = web3.utils.toBN(ownerBalanceBefore)
+
+            buyerBalanceBefore = await web3.eth.getBalance(accounts[1]);
+            buyerBalanceBefore = web3.utils.toBN(buyerBalanceBefore)
+        })
+
+        let receipt
+        let transaction
+
+        it("creates a token", async () => {
+            receipt = await collectible.claim(3, "Random text", { from: accounts[1], value: price })
+            transaction = await web3.eth.getTransaction(receipt.tx);
+        })
+
+        it("transfers ownership to the caller", async () => {
+            let owner = await collectible.ownerOf(2)
+            expect(owner).to.equal(accounts[1])
+        })
+
+        it("sets the text property", async () => {
+            let text = await collectible.getText(2)
+            expect(text).to.equal("Random text")
+        })
+
+        it("costs 0.01 ether plus gas to mint", async () => {
+            let buyerBalanceAfter = await web3.eth.getBalance(accounts[1])
+            buyerBalanceAfter = web3.utils.toBN(buyerBalanceAfter)
+            let gasCost = web3.utils.toBN(transaction.gasPrice * receipt.receipt.gasUsed)
+            let expectedBuyerBalance = buyerBalanceBefore.sub(price).sub(gasCost)
+            expect(buyerBalanceAfter.toString()).to.equal(expectedBuyerBalance.toString())
+        })
+
+        it("0.01 ether are transferred to the owners account", async () => {
+            let ownerBalanceAfter = await web3.eth.getBalance(accounts[0])
+            ownerBalanceAfter = web3.utils.toBN(ownerBalanceAfter)
+            let expectedOwnerBalance = ownerBalanceBefore.add(price)
+            expect(ownerBalanceAfter.toString()).to.equal(expectedOwnerBalance.toString())
+        })
+    })
+
+    describe("Trying to mint a date without paying", async () => {
+        it("fails", async () => {
+            await expectRevert(
+                collectible.claim(6, "The hacker"),
+                "Claiming a Collectible costs 0.01 ether"
+            )
+        })
+    })
+
+    describe("Changing the text of a collectible", async () => {
+        it("works if you are the owner", async () => {
+            await collectible.changeText(0, "First NFT edited", { from: accounts[0] })
+            let text = await collectible.getText(0)
+            expect(text).to.equal("First NFT edited")
+        })
+
+        it("reverts if you aren't the owner", async () => {
+            await expectRevert(
+                collectible.changeText(1, "Second NFT edited", { from: accounts[1] }),
+                "Only the owner of this NFT can change its text"
+            )
+
+            let text = await collectible.getText(1)
+            expect(text).to.equal("Second NFT")
+
+        })
+    })
+
+    describe("Changing the BaseURI", async () => {
+        it("works if you are the owner of the contract", async () => {
+            await collectible.setBaseURI("https://www.test.io/")
+            let tokenURI = await collectible.tokenURI(0)
+            expect(tokenURI).to.equal("https://www.test.io/0")
+        })
+
+        it("reverts if you are not the owner of the contract", async () => {
+            await expectRevert(
+                collectible.setBaseURI("https://www.test.com/", { from: accounts[1] }),
+                "caller is not the owner"
+            )
+
+            let tokenURI = await collectible.tokenURI(0)
+            expect(tokenURI).to.equal("https://www.test.io/0")
+
         })
     })
 });
